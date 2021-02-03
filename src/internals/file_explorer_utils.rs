dbf3e8ab1368ee120e872dde851c5ed557dade6d
artie <atch.cpp@gmail.com>
2021-02-03 12:26:01 +0100

    Removed original templates for a table

--- src/internals/file_explorer_utils.rs
+++ src/internals/file_explorer_utils.rs
@@ -175,51 +175,6 @@ impl TableViewItem<ExplorerColumn> for ExplorerColumnData {
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
-pub enum BasicColumn {
-    Name,
-    Count,
-    Rate,
-}
-
-impl BasicColumn {
-    fn as_str(&self) -> &str {
-        match *self {
-            BasicColumn::Name => "Name",
-            BasicColumn::Count => "Count",
-            BasicColumn::Rate => "Rate",
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct Foo {
-    name: String,
-    count: usize,
-    rate: usize,
-}
-
-impl TableViewItem<BasicColumn> for Foo {
-    fn to_column(&self, column: BasicColumn) -> String {
-        match column {
-            BasicColumn::Name => self.name.to_string(),
-            BasicColumn::Count => format!("{}", self.count),
-            BasicColumn::Rate => format!("{}", self.rate),
-        }
-    }
-
-    fn cmp(&self, other: &Self, column: BasicColumn) -> std::cmp::Ordering
-    where
-        Self: Sized,
-    {
-        match column {
-            BasicColumn::Name => self.name.cmp(&other.name),
-            BasicColumn::Count => self.count.cmp(&other.count),
-            BasicColumn::Rate => self.rate.cmp(&other.rate),
-        }
-    }
-}
-
 type tableViewType = TableView<ExplorerColumnData, ExplorerColumn>;
 pub fn create_basic_table_core(a_name: &'static str, initial_path: &PathBuf) -> NamedView<tableViewType> {
     let mut table = tableViewType::new()
