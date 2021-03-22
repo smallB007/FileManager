bf14e3bffbc4a9c55f7755300d33f8ae6cad7e3f
artie <atch.cpp@gmail.com>
2021-03-22 11:26:26 +0100

    Checking if  is set

--- src/internals/ops/f3_open.rs
+++ src/internals/ops/f3_open.rs
@@ -1,24 +1,31 @@
 use std::process::Command;
 
-use cursive::event::*;
 use cursive::CursiveExt;
+use cursive::{event::*, views::Dialog};
 
 use crate::internals::file_explorer_utils::{get_active_panel, get_selected_paths};
 
 pub fn open(siv: &mut cursive::Cursive) {
-    let active_panel = get_active_panel(siv);
-    match get_selected_paths(siv, &active_panel) {
-        Some(paths) => {
-            for path in paths {
-                let output = Command::new("nano")
-                    .arg(path.1)
-                    .status()
-                    .expect("failed to execute process");
+    if let Ok(editor) = std::env::var("EDITOR") {
+        let active_panel = get_active_panel(siv);
+        match get_selected_paths(siv, &active_panel) {
+            Some(paths) => {
+                for path in paths {
+                    let output = Command::new(editor.clone())
+                        .arg(path.1)
+                        .status()
+                        .expect("failed to execute process");
+                }
             }
+            None => {}
         }
-        None => {}
+        /*Funny enough, we need to add callback again...*/
+        siv.add_global_callback(Key::F3, open);
+        siv.run();
+    } else {
+        let dlg = Dialog::around(cursive::views::TextView::new("Please set editor first and try again"))
+            .title("No editor specified")
+            .dismiss_button("OK");
+        siv.add_layer(dlg);
     }
-    /*Funny enough, we need to add callback again...*/
-    siv.add_global_callback(Key::F3, open);
-    siv.run();
 }
