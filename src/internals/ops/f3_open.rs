923924b4b5e37525f3e55833dc2b38055fee4f71
artie <atch.cpp@gmail.com>
2021-03-22 11:01:18 +0100

    Starting external process with path

--- src/internals/ops/f3_open.rs
+++ src/internals/ops/f3_open.rs
@@ -2,14 +2,23 @@ use std::process::Command;
 
 use cursive::event::*;
 use cursive::CursiveExt;
+
+use crate::internals::file_explorer_utils::{get_active_panel, get_selected_paths};
+
 pub fn open(siv: &mut cursive::Cursive) {
-    let output = Command::new("nano")
-        //.stdout(Stdio::null())
-        //.arg("file.txt")
-        //.spawn()
-        //.output()
-        .status()
-        .expect("failed to execute process");
+    let active_panel = get_active_panel(siv);
+    match get_selected_paths(siv, &active_panel) {
+        Some(paths) => {
+            for path in paths {
+                let output = Command::new("nano")
+                    .arg(path.1)
+                    .status()
+                    .expect("failed to execute process");
+            }
+        }
+        None => {}
+    }
+    /*Funny enough, we need to add callback again...*/
     siv.add_global_callback(Key::F3, open);
     siv.run();
 }
